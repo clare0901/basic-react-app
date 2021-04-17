02d8a73c7822b2c01a0692bac3e1e3dd1817ba42
clare0901 <clarerebello0901@gmail.com>
2021-04-17 14:21:19 +0530

    change title

--- paint_app/src/components/RefreshButton.js
+++ paint_app/src/components/RefreshButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RefreshButton = ((props) =>{
     return(
-        <button className="refresh" onClick={props.cb}>
+        <button className="refresh" onClick={props.cb} title="Change Color" >
             <img src="https://img.icons8.com/windows/32/000000/refresh.png" alt="Refresh-Icon" />
         </button>
     )
--- paint_app/src/components/Save.js
+++ paint_app/src/components/Save.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Save = ((props) =>{
     // console.log(props.value.value);
     return(
-        <button className="save" onClick={props.cb}>
+        <button className="save" onClick={props.cb} title="Download" >
             <a id="downloads" download={props.value.value}></a>
             <img src="https://img.icons8.com/android/32/000000/download.png" alt="Download_Image" />
         </button>
